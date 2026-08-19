290b86fb3d586c0f2fa1f0729a205e04805717e2
Cássio Rubens <cassiorubensadv@gmail.com>
2026-08-19 15:44:17 -0300

    feat :: Configure environment and request validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
 })
 export class AppModule {}
