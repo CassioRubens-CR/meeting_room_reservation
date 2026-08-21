39546565922e9a3d222ba725a36cf329a39cc295
Cássio Rubens <cassiorubensadv@gmail.com>
2026-08-21 18:26:33 -0300

    feat :: Setup React Router with protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,47 @@
-import { HomePage } from './pages'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useAuthStore } from './store'
+import { LoginPage, RegisterPage, HomePage } from './pages'
+import { ProtectedRoute } from './components'
 
-function App() {
-  return <HomePage />
+export function App() {
+  const { hydrate } = useAuthStore()
+
+  // Hydrate auth state from localStorage on app start
+  useEffect(() => {
+    hydrate()
+  }, [hydrate])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Catch-all: redirect unknown routes to home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
